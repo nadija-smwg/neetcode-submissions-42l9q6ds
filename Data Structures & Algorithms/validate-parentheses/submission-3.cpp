f5bafa76b28ca78ee581f7ae1f279a668baadfed
Nadija Welihena Gamage <nadijasmwg@gmail.com>
2026-03-29 20:35:11 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.size() % 2 == 1) return false;
+
+        stack<char> st;
+        for(int i=0;i<s.size();i++) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') 
+                st.push(s[i]);
+            else{
+                if (st.empty()) return false;
+            
+                char top = st.top();
+                st.pop();
+                
+                if (top == '(' && s[i] != ')') return false;
+                if (top == '[' && s[i] != ']') return false;
+                if (top == '{' && s[i] != '}') return false;
+            }
+        }
+        return st.empty();
+        
+    }
+};
