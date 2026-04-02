45c922d8beadc7bcf13d6a1575b8c3aa22456199
Nadija Welihena Gamage <nadijasmwg@gmail.com>
2026-04-02 10:13:35 +0530

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        
+
+        //unordered_set<int> s;
+
+        // for(int num : nums) {
+        //     if (s.count(num)) {
+        //         return true;
+        //     }
+        //     s.insert(num);
+        // }
+        // return false;
+
+        // unordered_set<int> s(nums.begin(), nums.end());
+        // if ( nums.size() != s.size())
+        //     return true;
+        // else
+        //     return false;
+        
+    
+        return unordered_set<int>(nums.begin(), nums.end()).size() < nums.size();
+ 
+
+    }
+};
